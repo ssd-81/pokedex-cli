652a7dd9dad21669504079963b92150defb0b245
Sagar Singh <arcsagar.91@gmail.com>
2025-07-16 23:33:09 +0530

    implemented struct and map for CLI commands

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"github.com/ssd-81/pokedex-cli/repl"
 )
 
 func main() {
@@ -15,8 +16,13 @@ func main() {
 	for scanner.Scan() {
 		cleanText = strings.ToLower(scanner.Text())
 		slice := strings.Split(cleanText, " ")
-		// fmt.Printf("Your command was: %s", slice[0])
-		fmt.Print("Your command was: ", slice[0])
+		// fmt.Print("Your command was: ", slice[0])
+		if slice[0] == "exit" {
+			err := repl.CommandExit()
+			if err != nil {
+				fmt.Errorf("Error while exiting pokedex %")
+			}
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
--- repl/repl.go
+++ repl/repl.go
@@ -2,13 +2,37 @@ package repl
 
 import (
 	"strings"
+	"fmt"
+	"os"
 )
 
-func cleanInput(text string) []string {
+type CommandMap struct {
+	name	string
+	description string 
+	callback	func() error
+}
+
+// correct this structure 
+var cliMap = map[string]CommandMap {
+	"exit": {
+		name: "exit",
+		description: "Exit the Pokedex",
+		callback: CommandExit,
+	},
+}
+
+func CleanInput(text string) []string {
 	wordSlice := strings.Split(text, " ")
 	// fmt.Println(wordSlice)
 	for v, word := range wordSlice {
 		wordSlice[v] = strings.TrimSpace(word)
 	}
 	return wordSlice
+}
+
+func CommandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil 
+	
 }
