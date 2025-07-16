9d0147250ff246514ff2caefc70db6b601a4ed2e
Sagar Singh <arcsagar.91@gmail.com>
2025-07-16 22:23:05 +0530

    introduced basic REPL functionality and tested

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// NewScanner()
+	scanner := bufio.NewScanner(os.Stdin)
+	// --- for continued blocking ---
+	var cleanText string
+	for scanner.Scan() {
+		cleanText = strings.ToLower(scanner.Text())
+		slice := strings.Split(cleanText, " ")
+		// fmt.Printf("Your command was: %s", slice[0])
+		fmt.Print("Your command was: ", slice[0])
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error while reading input")
+	}
+
 }
