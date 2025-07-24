dafb79264167afe46a3e20f82a74684130a7c9ef
Sagar Singh <arcsagar.91@gmail.com>
2025-07-24 23:59:46 +0530

    day end commit for saving state

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -2,7 +2,7 @@ package pokecache
 
 import ("sync"
 		"time"
-		"fmt")
+		)
 
 type Cache struct {
 	cacheMap map[string]CacheEntry
@@ -15,43 +15,40 @@ type CacheEntry struct {
 	val []byte
 }
 
-func reapLoop(cache *Cache) {
-	// use the interval time to check if the a cacheEntry has expired, if yes
-	// then remove the entry from the cacheMap
-	ticker := time.NewTicker(cache.interval) 
+func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval) 
 	for {
 		select {
 		case <-ticker.C:   // go is very strict 
-		 	cache.mutex.Lock()
-			for key, value := range cache.cacheMap {
-				if value.createdAt.Add(cache.interval).Before(time.Now()){
-					delete(cache.cacheMap, key)
+		 	c.mutex.Lock()
+			for key, value := range c.cacheMap {
+				if value.createdAt.Add(c.interval).Before(time.Now()){
+					delete(c.cacheMap, key)
 				}
 			}
-			cache.mutex.Unlock()
+			c.mutex.Unlock()
 		}
-	}
-		
+	}	
 }
 
 func NewCache(interval time.Duration) *Cache {
 	cachePointer := &Cache{}
 	cachePointer.cacheMap = make(map[string]CacheEntry)
-	go reapLoop(cachePointer)
+	go cachePointer.reapLoop()
 	return cachePointer
 }
 
-func (c Cache) Add(key string, val []byte) {
+func (c *Cache) Add(key string, val []byte) {
 	c.mutex.Lock()
-	c.mutex.Unlock()
+	defer c.mutex.Unlock()
 	newCache := CacheEntry{time.Now(), val}
 	c.cacheMap[key] = newCache
 
 }
 
-func (c Cache) Get(key string) (val []byte, success bool){
+func (c *Cache) Get(key string) (val []byte, success bool){
 	c.mutex.Lock()
-	c.mutex.Unlock()
+	defer c.mutex.Unlock()
 	value, exists := c.cacheMap[key]
 	if exists {
 		return value.val, true
